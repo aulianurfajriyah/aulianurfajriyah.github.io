32a1296aef30aaaa7d8d2150f6e1e72df2b2ec28
aulianurfajriyah <aulia.nur.fajriyah@mail.ugm.ac.id>
2026-02-26 15:53:27 +0700

    Updates

--- _next/static/fANc3x0updRIiWJOMdupH/_buildManifest.js
+++ _next/static/fANc3x0updRIiWJOMdupH/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/aulianurfajriyah.github.io//_next/:path+",
-        "destination": "/aulianurfajriyah.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
